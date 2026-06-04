2004cc9e5ea0ec00337dd75e1da84d33e3d5d043
Aashin <aashinwebglaze@gmail.com>
2026-06-04 15:59:09 +0530

    logo-new

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,45 +1,40 @@
 // src/components/Navbar.tsx
 import Image from 'next/image'
-import Link from "next/link"
+import Link from 'next/link'
 import { getSiteSettings } from '@/lib/payload-utils'
+
+// adjust the path if needed
+import logo from '../../media/hitayu-1.png'
 
 export default async function Navbar() {
   const settings = await getSiteSettings()
   const siteName = settings?.siteName || 'HOSTIKO'
-  const navItems = settings?.navigation || []
-  const localImageUrl =  "https://madebydesignesia.com/themes/hostco/images/logo.png"
 
   return (
     <nav className="navbar">
       <div className="container navbar-wrapper">
         <div className="navbar-start">
           <div className="logo-container">
-            {settings?.logo?.url ? (
-              <Image
-                // src={settings.logo.url}
-                src={localImageUrl}
-                alt="Logo"
-                width={249}
-                height={50}
-              />
-            ) : (
-              <>
-                <div className="logo">{siteName}</div>
-              </>
-            )}
+            <Image
+              src={logo}
+              alt="Logo"
+              width={249}
+              height={50}
+              priority
+            />
           </div>
         </div>
 
         <ul className="nav-links">
-          {settings?.navigation?.length ? (
-            settings.navigation.map((item: any, index: number) => (
-              <li key={index}>
-                <Link href={item.url} className="nav-link">
-                  {item.label}
-                </Link>
-              </li>
-            ))
-          ) : null}
+          {settings?.navigation?.length
+            ? settings.navigation.map((item: any, index: number) => (
+                <li key={index}>
+                  <Link href={item.url} className="nav-link">
+                    {item.label}
+                  </Link>
+                </li>
+              ))
+            : null}
         </ul>
       </div>
     </nav>
