900ddbdbdc44cd1960106a3a12ddd09554651427
Aashin <aashinwebglaze@gmail.com>
2026-06-06 17:26:08 +0530

    menu changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,40 @@
-// src/components/Navbar.tsx
 import Image from 'next/image'
 import Link from 'next/link'
 import { getSiteSettings } from '@/lib/payload-utils'
+import MobileMenu from './MobileMenu'
 
 export default async function Navbar() {
   const settings = await getSiteSettings()
+
   const siteName = settings?.siteName || 'HOSTIKO'
   const navItems = settings?.navigation || []
+
   const rawLogoUrl = settings?.logo?.url
+
   const logoUrl =
-    rawLogoUrl && !rawLogoUrl.startsWith('/api/media/file/') ? rawLogoUrl : '/hitayu-1.png'
+    rawLogoUrl && !rawLogoUrl.startsWith('/api/media/file/')
+      ? rawLogoUrl
+      : '/hitayu-1.png'
+
   const headerButton = settings?.headerButton
 
   return (
     <nav className="navbar">
       <div className="container navbar-wrapper">
-        <div className="navbar-start">
-          <div className="logo-container">
-            <Image src={logoUrl} alt={`${siteName} logo`} width={200} height={30} />
-          </div>
+        {/* Logo */}
+        <div className="logo-container">
+          <Link href="/">
+            <Image
+              src={logoUrl}
+              alt={`${siteName} logo`}
+              width={200}
+              height={60}
+              priority
+            />
+          </Link>
         </div>
 
+        {/* Desktop Navigation */}
         <div className="navbar-menu">
           <ul className="nav-links">
             {navItems.map((item: any, index: number) => (
@@ -48,6 +62,12 @@ export default async function Navbar() {
             </Link>
           )}
         </div>
+
+        {/* Mobile Navigation */}
+        <MobileMenu
+          navItems={navItems}
+          headerButton={headerButton}
+        />
       </div>
     </nav>
   )
